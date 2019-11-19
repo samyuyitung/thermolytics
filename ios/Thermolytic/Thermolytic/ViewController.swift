52d38afc8086c8d84ab46395abf544fbb50f2e49
samyuyitung <samyuyitung@gmail.com>
2019-11-19 18:25:49 -0500

    new app

---
+++ ios/Thermolytic/Thermolytic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thermolytic
+//
+//  Created by Sam Yuyitung on 2019-11-19.
+//  Copyright © 2019 Sam Yuyitung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
